3cbe45d794ab4556bf2de839dbb75c26c39bbdf4
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-06-28 16:14:31 -0400

    Add functions to create new tasks and save to local storage

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ const deadlineInput = document.getElementById("task-deadline");
 const statusInput = document.getElementById("task-status");
 const addBtn = document.getElementById("add-task");
 const taskList = document.getElementById("task-list");
+
+function createTask(name, category, deadline, status = "In Progress") {
+  return { id: Date.now(), name, category, deadline, status };
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
